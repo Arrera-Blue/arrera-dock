7afb58cf0c246a8f5b334a5079c39c513f09251b
baptiste p <baptiste.pauchet13@gmail.com>
2026-09-14 11:36:49 +0200

    Optimisation du lauch dev

--- lauch_dev.sh
+++ lauch_dev.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Recompiler les schémas GSettings locaux
+glib-compile-schemas schemas/
+
+# S'assurer que l'extension est activée dans la liste des extensions
+gsettings set org.gnome.shell enabled-extensions "['dock@linux.arrera-software.fr']"
+
+# Lancer la session de test GNOME Shell
 dbus-run-session -- gnome-shell --devkit
